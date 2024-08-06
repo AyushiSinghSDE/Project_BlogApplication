6826ac5907a98a27479d3ca25e2eb5e27fcc0683
AyushiSingh <singh.ayushi.ddn.2000@gmail.com>
2024-08-07 01:31:44 +0530

    Adding getter setter in PostService

--- src/main/java/com/ayushi/BlogApplication/services/PostService.java
+++ src/main/java/com/ayushi/BlogApplication/services/PostService.java
@@ -9,7 +9,7 @@ import com.ayushi.BlogApplication.payloads.PostDto;
 @Service
 public interface PostService {
 	
-	PostDto createPost(PostDto postDto);
+	PostDto createPost(PostDto postDto, Integer userId, Integer categoryId);
 	PostDto updatePost(PostDto postDto, Integer postId);
 	PostDto getPostById(Integer postId);
 	List<PostDto> getAllPosts();
